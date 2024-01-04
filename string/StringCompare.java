6d13c4543c4bdde87115b86f36aacf875a50acf6
Jasonarea <hyeonsuns@naver.com>
2024-01-04 23:11:44 +0900

    Recommit and push

--- string/StringCompare.java
+++ string/StringCompare.java
@@ -1,2 +1,45 @@
-package string;public class StringCompare {
+package string;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        StringCompare sample = new StringCompare();
+        sample.checkString();
+        sample.checkCompare();
+        sample.checkCompreTo();
+    }
+    public void checkString() {
+        String text = "안녕 나는 한글이야";
+        System.out.println("text.length() = " + text.length());
+        System.out.println("text.isEmpty() = " + text.isEmpty());
+    }
+
+    public void checkCompare() {
+        String text = "Check value";
+        String text2 = new String("Check value");
+
+        if(text == text2)  {
+            System.out.println("text == text2 result is same");
+        } else {
+            System.out.println("text == text2 result is different");
+        }
+
+        if(text.equals("Check value")) {
+            System.out.println("text.equals(text2) result is same.");
+        }
+
+        String text3 = "check value";
+        if(text.equalsIgnoreCase(text3)) {
+            System.out.println("text.equalsIgnoreCase(text) result is same");
+        }
+
+    }
+
+    public void checkCompreTo() {
+        String text = "a";
+        String text2 = "b";
+        String text3 = "c";
+        System.out.println(text2.compareTo(text));
+        System.out.println(text2.compareTo(text3));
+        System.out.println(text.compareTo(text3));
+    }
 }
--- string/practice/UseStringMethod.java
+++ string/practice/UseStringMethod.java
@@ -1,2 +1,49 @@
-package string.practice;public class UseStringMethod {
+package string.practice;
+
+public class UseStringMethod {
+    public static void main(String[] args) {
+        UseStringMethod method = new UseStringMethod();
+        String text = "The String class represents character strings";
+        method.printWords(text);
+        method.findString(text, "string");
+        method.findAnyCaseString(text, "string");
+        method.countChar(text, 's');
+        method.printContainWords(text, "ss");
+    }
+
+    public void printWords(String str) {
+        String []splits = str.split(" ");
+        for(String temp : splits) {
+            System.out.println(temp);
+        }
+    }
+
+    public void findString(String str, String findStr) {
+        int idx = str.indexOf(findStr);
+        System.out.println(findStr + " is appeared at " + idx);
+    }
+
+    public void findAnyCaseString(String str, String findStr) {
+        String lowerConvertedStr = str.toLowerCase();
+        int idx = lowerConvertedStr.indexOf(findStr);
+        System.out.println(findStr + " is appeared at " + idx);
+    }
+
+    public void countChar(String str, char c) {
+        int count = 0;
+        char []charArray = str.toCharArray();
+        for(int i = 0;i<str.length();i++) {
+            if(charArray[i] == c) count++;
+        }
+        System.out.println("char " + c + " count is " + count);
+    }
+
+    public void printContainWords(String str, String findStr) {
+        String []splits = str.split(" ");
+        for(String temp : splits) {
+            if(temp.contains(findStr)) {
+                System.out.println(temp + " contains " + findStr);
+            }
+        }
+    }
 }
